9e9df5239f658143a2e581cec44e1e7b10330c11
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2025-02-23 01:29:39 +0800

    chore: update maxBlocksToQueryByChain

--- server/src/utils/constants.ts
+++ server/src/utils/constants.ts
@@ -2,51 +2,15 @@ export const PRICES_API = "https://coins.llama.fi/prices";
 
 export const defaultConfidenceThreshold = 0.5; // for querying defillama prices
 
-// each chain should have number of blocks per approx. 1.5 or 2 hours. default is 400.
 export const maxBlocksToQueryByChain = {
   default: 400,
-  ethereum: 400,
+  ethereum: 500,
   polygon: 2000,
-  fantom: 5000,
+  base: 2000,
   arbitrum: 25000,
   avalanche: 3000,
   avax: 3000,
   bsc: 2000,
   optimism: 12000,
   gnosis: 400,
-  aurora: 5400,
-  celo: 1200,
-  klaytn: 6000,
 } as { [chain: string]: number };
-
-export const nonBlocksChains = [
-  "osmosis",
-  "secret",
-  "injective",
-  "terra",
-  "crescent",
-  "juno",
-  "kujira",
-  "sifchain",
-  "stride",
-  "cosmos",
-  "canto",
-];
-/*
-// for slow adapters
-export const maxBlocksToQueryByChain = {
-  default: 1600,
-  ethereum: 1600,
-  polygon: 8000,
-  fantom: 20000,
-  arbitrum: 40000,
-  avalanche: 12000,
-  avax: 12000,
-  bsc: 8000,
-  optimism: 40000,
-  gnosis: 1200,
-  aurora: 10000,
-  celo: 3000,
-  klaytn: 8000,
-} as { [chain: string]: number };
-*/
