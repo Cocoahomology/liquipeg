1c5d33e79d9b7701690056413f2d4b90e0afc6ad
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2025-03-25 16:42:03 +0800

    chore: move fillMissingBlockTimestamps

--- server/src/utils/blocks.ts
+++ server/src/utils/blocks.ts
@@ -1,13 +1,8 @@
-import * as sdk from "@defillama/sdk";
 import { getLatestBlock } from "@defillama/sdk/build/util";
 import { Block } from "@defillama/sdk/build/util/blocks";
 import { withTimeout } from "./async";
 import { ErrorLoggerService } from "./bunyan";
-import { getBlocksWithMissingTimestamps } from "../db/read";
 import retry from "async-retry";
-import { PromisePool } from "@supercharge/promise-pool";
-import { insertBlockTimestampEntries } from "../db/write";
-import { maxBlocksToQueryByChain } from "./constants";
 
 export async function getLatestBlockWithLogging(
   chain: string,
@@ -35,62 +30,4 @@ export async function getLatestBlockWithLogging(
     logger.error(logData);
     throw error;
   }
-}
-
-// Fix: Move to Handlers
-export async function fillMissingBlockTimestamps() {
-  const blockTimestampEntries = await getBlocksWithMissingTimestamps();
-
-  await Promise.allSettled(
-    Object.entries(blockTimestampEntries).map(async ([chain, blockEntries]) => {
-      const provider = await sdk.getProvider(chain);
-      const blockTimestamps = {} as Record<number, number>;
-      const minBlockInterval = Math.floor((maxBlocksToQueryByChain[chain] || maxBlocksToQueryByChain.default) / 10);
-
-      let lastTimestamp: number | null = null;
-      let lastBlockNumber: number | null = null;
-
-      await PromisePool.for(blockEntries)
-        .withConcurrency(2) // FIX: adjust
-        .process(async (entry) => {
-          const blockNumber = entry.blockNumber;
-
-          if (!blockTimestamps[blockNumber]) {
-            if (
-              lastBlockNumber !== null &&
-              lastTimestamp !== null &&
-              blockNumber - lastBlockNumber < minBlockInterval
-            ) {
-              // Reuse the last timestamp for nearby blocks
-              blockTimestamps[blockNumber] = lastTimestamp;
-            } else {
-              try {
-                await new Promise((resolve) => setTimeout(resolve, Math.random() * 1000));
-                const block = await retry(
-                  async () => withTimeout(provider.getBlock(blockNumber), { milliseconds: 15000 }),
-                  // FIX: adjust
-                  {
-                    retries: 1,
-                    minTimeout: 1000,
-                    maxTimeout: 2000,
-                  }
-                );
-                if (block) {
-                  blockTimestamps[blockNumber] = Number(block.timestamp);
-                  lastTimestamp = Number(block.timestamp);
-                  lastBlockNumber = blockNumber;
-                }
-              } catch (error) {
-                // this is going to timeout all the time
-              }
-            }
-          }
-        });
-      const blockTimestampsList = Object.entries(blockTimestamps).map(([blockNumber, timestamp]) => ({
-        blockNumber: Number(blockNumber),
-        timestamp,
-      }));
-      await insertBlockTimestampEntries(chain, blockTimestampsList);
-    })
-  );
 }
