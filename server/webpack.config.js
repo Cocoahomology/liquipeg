046922c866a87069188d5fc89ad8a046db4ffaec
Cocoahomology <83151536+Cocoahomology@users.noreply.github.com>
2025-04-01 02:05:48 +0800

    build: update webpack config

--- server/webpack.config.js
+++ server/webpack.config.js
@@ -1,25 +1,40 @@
 const slsw = require("serverless-webpack");
 const path = require("path");
+const nodeExternals = require("webpack-node-externals");
 
 module.exports = {
   entry: slsw.lib.entries,
   target: "node",
   mode: slsw.lib.webpack.isLocal ? "development" : "production",
+  externals: [
+    nodeExternals({
+      allowlist: [/^(?!aws-sdk)/]
+    })
+  ],
+  performance: {
+    hints: false,
+  },
+  optimization: {
+    minimize: false, // Lambda doesn't need minimized code
+    removeAvailableModules: false,
+    removeEmptyChunks: false,
+    splitChunks: false,
+  },
   module: {
     rules: [
       {
         test: /\.ts$/,
-        use: "ts-loader",
+        use: {
+          loader: "ts-loader",
+          options: {
+            compilerOptions: {
+              skipLibCheck: true,
+              transpileOnly: true,
+            },
+          },
+        },
         include: path.resolve(__dirname, "src"),
         exclude: /node_modules/,
-      },
-      {
-        test: /\.js$/,
-        include: __dirname,
-        exclude: /node_modules/,
-        use: {
-          loader: "babel-loader",
-        },
       },
       {
         test: /\.mjs$/,
